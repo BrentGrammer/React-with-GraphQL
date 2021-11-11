8728d5525abaeb1a29836d5f2271a32898916940
Brent Marquez <brentonmarquez@gmail.com>
2021-11-10 20:26:19 -0700

    Create global styles with styled comonents

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,12 +1,67 @@
 import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
+/**
+Can use global styles api for global css settings.
+ 
+  Inject the styles by using this as a component in your upper level heriarchy
+ */
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey); // easy spelling fallback
+    --lightGrey: #e1e1e1;
+    --lightGray: var(---lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);  // box shadow global
+  }
+  *, *:before, *:after {
+    box-sizing: inherit; // prevent scrolling bugs - take away size when adding padding etc to components
+  }
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: var(---black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button: { // setting font family on body does not apply to buttons so have to set it here again.
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+// We inject global styles here above the header
 export default function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      page template
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
