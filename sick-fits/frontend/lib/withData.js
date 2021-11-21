ede75215e5c611ce9b224bc97129b17812a96094
Brent Marquez <brentonmarquez@gmail.com>
2021-11-20 19:50:22 -0700

    Setup Apollo Client for GraphQL on frontend React

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -5,9 +5,19 @@ import { createUploadLink } from 'apollo-upload-client';
 import withApollo from 'next-with-apollo';
 import { endpoint, prodEndpoint } from '../config';
 
+/**
+ *
+ * Required for supporting image upload with Apollo Client.
+ * This is a secondary link that is not included with Apollo-Boost package, so in order to support image uploads,
+ * we need to eject from Apollo Boost and add this boilerplate code.
+ *
+ * This export is a withData wrapper we use in _app.js to inject the apollo data to apollo provider
+ */
+
 function createClient({ headers, initialState }) {
   return new ApolloClient({
     link: ApolloLink.from([
+      // error handling link - useful to log out graphql or network errors for debugging.
       onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors)
           graphQLErrors.forEach(({ message, locations, path }) =>
@@ -21,15 +31,18 @@ function createClient({ headers, initialState }) {
           );
       }),
       // this uses apollo-link-http under the hood, so all the options here come from that package
+      // createUploadLink is built ontop of popular http link and it adds file upload handling
       createUploadLink({
         uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
         fetchOptions: {
           credentials: 'include',
         },
         // pass the headers along from this request. This enables SSR with logged in state
-        headers,
+        headers, // sends cookies and login info for user to prevent flicker on refresh.
+        // *** Our JWT is in a cookie instead of local storage so it can be sent to the server so it renders back the logged in user pages
       }),
     ]),
+    // im memory storage is in the browser in this case
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
@@ -39,8 +52,9 @@ function createClient({ headers, initialState }) {
           },
         },
       },
-    }).restore(initialState || {}),
+    }).restore(initialState || {}), // restore state back to client on refreshes if it exists
   });
 }
 
+// this will crawl all queries and make sure all are resolved and fetched before sending back data to client for rendering from server
 export default withApollo(createClient, { getDataFromTree });
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,7 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import NProgress from 'nprogress';
+import { ApolloProvider } from '@apollo/client';
 import Router from 'next/router';
 import Page from '../components/Page';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 // Import next.js router to hook into it to run nprogress animation based on events
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -13,10 +17,28 @@ Router.events.on('routeChangeError', () => NProgress.done());
     This component will wrap all of our pages now. 
 */
 
-export default function MyApp({ Component, pageProps }) {
+// apollo is coming from wrapping the upper level with a withData wrapper
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// next.js specific config for working with apollo
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  // pages will have getInitialProps on them added by `withData` wrapper
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  // get query params in a url at a page level
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+// withData was created to use apollo boost packages with file upload capability - see file
+export default withData(MyApp);
