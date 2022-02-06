466b7f0b8e2c929f7f9b5f7e8841e824a03323ed
Brent Marquez <brentonmarquez@gmail.com>
2022-02-05 21:03:14 -0700

    fix initial state bug in useForm

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -1,8 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useForm(initial = {}) {
   // state obj for inputs
   const [inputs, setInputs] = useState(initial);
+  // watch values on the initial to prevent an infinite loop (if you just watch initial itself which gets overwritten and replaced with a new object triggering the useEffect over and over again)
+  // This was done to prevent the edit product/update form not getting state set initially and causing a bug.
+  const initialValues = Object.values(initial).join(''); // goes from a string of nothing(loading) to a string of values when data is present
+  // update the initial state of the form (in the case of it being an update form)
+  // when undefined state of loading changes to having actual data, you need to update initial state with that data
+  useEffect(() => {
+    // we watch a string conversion of initial to prevent setInputs causing an infinite loop when initial is replaced (triggering another change)
+    setInputs(initial);
+  }, [initialValues]);
 
   function handleChange(e) {
     let { value, name, type } = e.target;
