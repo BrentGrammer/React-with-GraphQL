81d3caf6ee994cdb567cde4311c28e48275acda6
Brent Marquez <brentonmarquez@gmail.com>
2022-05-15 19:36:49 -0600

    Signup flow

---
+++ sick-fits/frontend/components/SignUp.js
@@ -0,0 +1,86 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import Error from './ErrorMessage';
+
+// note: createUser is provided by Keystone
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      email
+      name
+      password
+    }
+  }
+`;
+
+export default function SignUp() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs,
+    // // refetch currently logged in user
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await signup().catch(console.error); // catch to prevent warning - error is in above useMutation returns.
+    resetForm();
+  }
+
+  return (
+    // Put POST method on form to make sure fields like passwords don't go into the URL that's shown requested.
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Up for an Account</h2>
+      <Error error={error} />
+      <fieldset>
+        {data?.createUser && (
+          <p>Signed up with {data.createUser.email} - Sign in!</p>
+        )}
+        <label htmlFor="email">
+          name
+          <input
+            type="name"
+            name="name"
+            placeholder="Your name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          password
+          <input
+            type="password"
+            name="password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign in</button>
+      </fieldset>
+    </Form>
+  );
+}
